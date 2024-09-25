d7c8bd8088cc4d90a40bd49fc054f8bef3d33f1b
otteru <otteru@naver.com>
2024-09-25 21:09:17 +0900

    SessionManagerTest 수정해야 함

---
+++ src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,33 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    void sessionTest() {
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        //세션 생성
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        // 요청에 응답 쿠키 저장
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        // 세션 조회
+        Object result = sessionManager.getSession(request);
+        Assertions.assertThat(result).isEqualTo(member);
+
+    }
+}
