8a371c271348a8fa1efcb47e02a9a6b62ca9e728
otteru <otteru@naver.com>
2024-09-30 19:27:45 +0900

    done

--- src/main/java/hello/login/web/argumentResolver/LoginMemberArgumentResolver.java
+++ src/main/java/hello/login/web/argumentResolver/LoginMemberArgumentResolver.java
@@ -1,12 +1,16 @@
 package hello.login.web.argumentResolver;
 
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
@@ -23,7 +27,14 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     @Override
     public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
 
+        log.info("resolveArgument 실행");
 
-        return null;
+        HttpServletRequest request = (HttpServletRequest) nativeWebRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if(session == null) {
+            return null;
+        }
+
+        return session.getAttribute(SessionConst.LOGIN_MEMBER);
     }
 }
